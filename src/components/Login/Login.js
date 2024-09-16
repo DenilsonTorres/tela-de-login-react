574fc90281d97be7dbb87fae4dc1be2d327dc3e1
Denilson Torres <dtlima2020@gmail.com>
2024-09-16 07:31:21 -0300

    created-login-screen

---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+function Login() {
+  return (
+    <div>
+      <h2>Acessar a plataforma</h2>
+      <div>
+        <input type="email" placeholder="E-mail" />
+      </div>
+      <div>
+        <input type="password" placeholder="Senha" />
+      </div>
+      <button>Entrar</button>
+    </div>
+  );
+}
+
+export default Login;
